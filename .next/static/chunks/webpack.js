b26182d9c518a5dfa70366c327cfa939e03521e5
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-02-21 01:41:26 -0500

    done for the night, got thru 1/3 of temrs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0cdca4d2bb673ff3d608";
+/******/ 	var hotCurrentHash = "0db0f2fed77b3a979041";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Sections/Legal/TermsText.js
+++ src/components/Sections/Legal/TermsText.js
@@ -9,11 +9,150 @@ export default function TermsText(style) {
         </SectionTitleOne>
         <div className="post-content__body">
           <p className="post-paragraph">
-          PLEASE READ THE FOLLOWING TERMS OF SERVICE AGREEMENT CAREFULLY. BY ACCESSING OR USING OUR SITES AND OUR SERVICES, YOU HEREBY AGREE TO BE BOUND BY THE TERMS AND ALL TERMS INCORPORATED HEREIN BY REFERENCE. IT IS THE RESPONSIBILITY OF YOU, THE USER, CUSTOMER, OR PROSPECTIVE CUSTOMER TO READ THE TERMS AND CONDITIONS BEFORE PROCEEDING TO USE THIS SITE. IF YOU DO NOT EXPRESSLY AGREE TO ALL OF THE TERMS AND CONDITIONS, THEN PLEASE DO NOT ACCESS OR USE OUR SITES OR OUR SERVICES. THIS TERMS OF SERVICE AGREEMENT IS EFFECTIVE AS OF 06/28/2020.
+            PLEASE READ THE FOLLOWING TERMS OF SERVICE AGREEMENT CAREFULLY. BY ACCESSING OR USING OUR SITES AND OUR SERVICES, YOU HEREBY AGREE TO BE BOUND BY THE TERMS AND ALL TERMS INCORPORATED HEREIN BY REFERENCE. IT IS THE RESPONSIBILITY OF YOU, THE USER, CUSTOMER, OR PROSPECTIVE CUSTOMER TO READ THE TERMS AND CONDITIONS BEFORE PROCEEDING TO USE THIS SITE. IF YOU DO NOT EXPRESSLY AGREE TO ALL OF THE TERMS AND CONDITIONS, THEN PLEASE DO NOT ACCESS OR USE OUR SITES OR OUR SERVICES. THIS TERMS OF SERVICE AGREEMENT IS EFFECTIVE AS OF 06/28/2020.
+          </p>
+          {/* ACCEPTANCE */}
+          <p className="post-paragraph">
+          <span>Acceptance of Terms</span><br />
+            The following Terms of Service Agreement (the "TOS") is a legally binding agreement that shall govern the relationship with our users and others which may interact or interface with Yego Coffee, also known as Yego Coffee, located at 142 Lexington Street, Woburn, MA 01801 and our subsidiaries and affiliates, in association with the use of the Yego Coffee website, which includes www.yegocoffee.com, (the "Site") and its Services, which shall be defined below.
+          </p>
+          {/* DESCRIPTION */}
+          <p className="post-paragraph">
+          <span>Description of Website Services Offered</span><br />
+            The Site is an e-commerce business website which has the following description:
           </p>
           <p className="post-paragraph">
-          Buying your genuine Rwandan coffee from Yego Coffee will make a huge impact on those who need it the most.  Every little bit counts!
+            The Yego Coffee website (the “Site”) is a website used to promote and provide information on the services and products offered by the Yego Coffee website and company.
           </p>
+          <p className="post-paragraph">
+            Any and all visitors to our site, despite whether they are registered or not, shall be deemed as "users" of the herein contained Services provided for the purpose of this TOS. Once an individual register's for our Services, through the process of creating an account, the user shall then be considered a "member."
+          </p>
+          <p className="post-paragraph">
+            The user and/or member acknowledges and agrees that the Services provided and made available through our website and applications, which may include some mobile applications and that those applications may be made available on various social media networking sites and numerous other platforms and downloadable programs, are the sole property of Yego Coffee. At its discretion, Yego Coffee may offer additional website Services and/or products, or update, modify or revise any current content and Services, and this Agreement shall apply to any and all additional Services and/or products and any and all updated, modified or revised Services unless otherwise stipulated. Yego Coffee does hereby reserve the right to cancel and cease offering any of the aforementioned Services and/or products. You, as the end user and/or member, acknowledge, accept and agree that Yego Coffee shall not be held liable for any such updates, modifications, revisions, suspensions or discontinuance of any of our Services and/or products. Your continued use of the Services provided, after such posting of any updates, changes, and/or modifications shall constitute your acceptance of such updates, changes and/or modifications, and as such, frequent review of this Agreement and any and all applicable terms and policies should be made by you to ensure you are aware of all terms and policies currently in effect. Should you not agree to the updated, revised or modified terms, you must stop using the provided Services forthwith.
+          </p>
+          <p className="post-paragraph">
+            Furthermore, the user and/or member understands, acknowledges and agrees that the Services offered shall be provided "AS IS" and as such Yego Coffee shall not assume any responsibility or obligation for the timeliness, missed delivery, deletion and/or any failure to store user content, communication or personalization settings.
+          </p>
+          {/* REGISTRATION */}
+          <p className="post-paragraph">
+            <span>Registration</span><br />
+            To register and become a "member" of the Site, you must be at least 18 years of age to enter into and form a legally binding contract. In addition, you must be in good standing and not an individual that has been previously barred from receiving Yego Coffee's Services under the laws and statutes of the United States or other applicable jurisdiction.
+          </p>
+          <p className="post-paragraph">
+            When you register, Yego Coffee may collect information such as your name, e-mail address, birth date, gender, mailing address, occupation, industry and personal interests. You can edit your account information at any time. Once you register with Yego Coffee and sign in to our Services, you are no longer anonymous to us.
+          </p>
+          <p className="post-paragraph">
+            Furthermore, the registering party hereby acknowledges, understands and agrees to:
+          </p>
+          <p className="post-paragraph">
+            <ol>
+                <li>
+                    furnish factual, correct, current and complete information with regards to yourself as may be requested by the data registration process, and
+                </li>
+                <li>
+                    maintain and promptly update your registration and profile information in an effort to maintain accuracy and completeness at all times.
+                </li>
+            </ol>
+          </p>
+          <p className="post-paragraph">
+            If anyone knowingly provides any information of a false, untrue, inaccurate or incomplete nature, Yego Coffee will have sufficient grounds and rights to suspend or terminate the member in violation of this aspect of the Agreement, and as such refuse any and all current or future use of Yego Coffee Services, or any portion thereof.
+          </p>
+          <p className="post-paragraph">
+            It is Yego Coffee's priority to ensure the safety and privacy of all its visitors, users and members, especially that of children. Therefore, it is for this reason that the parents of any child under the age of 13 that permit their child or children access to the Yego Coffee website platform Services must create a "family" account, which will certify that the individual creating the "family" account is of 18 years of age and as such, the parent or legal guardian of any child or children registered under the "family" account. As the creator of the "family" account, s/he is thereby granting permission for his/her child or children to access the various Services provided, including, but not limited to, message boards, email, and/or instant messaging. It is the parent's and/or legal guardian's responsibility to determine whether any of the Services and/or content provided are age-appropriate for his/her child.
+          </p>
+          {/* PRIVACY */}
+          <p className="post-paragraph">
+            <span>Privacy Policy</span><br />
+            Every member's registration data and various other personal information are strictly protected by the Yego Coffee Online Privacy Policy (see the full <a href="/legal/privacy">Privacy Policy</a>). As a member, you herein consent to the collection and use of the information provided, including the transfer of information within the United States and/or other countries for storage, processing or use by Yego Coffee and/or our subsidiaries and affiliates.
+          </p>
+          {/* MEMBERS */}
+          <p className="post-paragraph">
+            <span>Member Account, Username, Password, and Security</span><br />
+            When you set up an account, you are the sole authorized user of your account. You shall be responsible for maintaining the secrecy and confidentiality of your password and for all activities that transpire on or within your account. It is your responsibility for any act or omission of any user(s) that access your account information that, if undertaken by you, would be deemed a violation of the TOS. It shall be your responsibility to notify Yego Coffee immediately if you notice any unauthorized access or use of your account or password or any other breach of security. Yego Coffee shall not be held liable for any loss and/or damage arising from any failure to comply with this term and/or condition of the TOS.
+          </p>
+          {/* CONDUCT */}
+          <p className="post-paragraph">
+            <span>Conduct</span><br />
+            As a user or member of the Site, you herein acknowledge, understand and agree that all information, text, software, data, photographs, music, video, messages, tags or any other content, whether it is publicly or privately posted and/or transmitted, is the expressed sole responsibility of the individual from whom the content originated. In short, this means that you are solely responsible for any and all content posted, uploaded, emailed, transmitted or otherwise made available by way of the Yego Coffee Services, and as such, we do not guarantee the accuracy, integrity or quality of such content. It is expressly understood that by use of our Services, you may be exposed to content including, but not limited to, any errors or omissions in any content posted, and/or any loss or damage of any kind incurred as a result of the use of any content posted, emailed, transmitted or otherwise made available by Yego Coffee.
+          </p>
+          <p className="post-paragraph">
+            Furthermore, you herein agree not to make use of Yego Coffee's Services for the purpose of:
+          </p>
+          <p className="post-paragraph">
+            <ol>
+                <li>
+                    uploading, posting, emailing, transmitting, or otherwise making available any content that shall be deemed unlawful, harmful, threatening, abusive, harassing, tortious, defamatory, vulgar, obscene, libelous, or invasive of another's privacy or which is hateful, and/or racially, ethnically, or otherwise objectionable;
+                </li>
+                <li>
+                    causing harm to minors in any manner whatsoever;
+                </li>
+                <li>
+                    impersonating any individual or entity, including, but not limited to, any Yego Coffee officials, forum leaders, guides or hosts or falsely stating or otherwise misrepresenting any affiliation with an individual or entity;
+                </li>
+                <li>
+                    forging captions, headings or titles or otherwise offering any content that you personally have no right to pursuant to any law nor having any contractual or fiduciary relationship with;
+                </li>
+                <li>
+                    uploading, posting, emailing, transmitting or otherwise offering any such content that may infringe upon any patent, copyright, trademark, or any other proprietary or intellectual rights of any other party;
+                </li>
+                <li>
+                    uploading, posting, emailing, transmitting or otherwise offering any content that you do not personally have any right to offer pursuant to any law or in accordance with any contractual or fiduciary relationship;
+                </li>
+                <li>
+                    uploading, posting, emailing, transmitting, or otherwise offering any unsolicited or unauthorized advertising, promotional flyers, "junk mail," "spam," or any other form of solicitation, except in any such areas that may have been designated for such purpose;
+                </li>
+                <li>
+                    uploading, posting, emailing, transmitting, or otherwise offering any source that may contain a software virus or other computer code, any files and/or programs which have been designed to interfere, destroy and/or limit the operation of any computer software, hardware, or telecommunication equipment;
+                </li>
+                <li>
+                    disrupting the normal flow of communication, or otherwise acting in any manner that would negatively affect other users' ability to participate in any real time interactions;
+                </li>
+                <li>
+                    interfering with or disrupting any Yego Coffee Services, servers and/or networks that may be connected or related to our website, including, but not limited to, the use of any device software and/or routine to bypass the robot exclusion headers;
+                </li>
+                <li>
+                    intentionally or unintentionally violating any local, state, federal, national, or international law, including, but not limited to, rules, guidelines, and/or regulations decreed by the U.S. Securities and Exchange Commission, in addition to any rules of any nation or other securities exchange, that would include without limitation, the New York Stock Exchange, the American Stock Exchange, or the NASDAQ, and any regulations having the force of law;
+                </li>
+                <li>
+                    providing informational support or resources, concealing and/or disguising the character, location, and or source to any organization delegated by the United States government as a "foreign terrorist organization" in accordance to Section 219 of the Immigration Nationality Act;
+                </li>
+                <li>
+                    "stalking" or with the intent to otherwise harass another individual; and/or
+                </li>
+                <li>
+                    collecting or storing of any personal data relating to any other member or user in connection with the prohibited conduct and/or activities which have been set forth in the aforementioned paragraphs.
+                </li>
+            </ol>
+          </p>
+          <p className="post-paragraph">
+            Yego Coffee herein reserves the right to pre-screen, refuse and/or delete any content currently available through our Services. In addition, we reserve the right to remove and/or delete any such content that would violate the TOS or which would otherwise be considered offensive to other visitors, users and/or members.
+          </p>
+          <p className="post-paragraph">
+            Yego Coffee herein reserves the right to access, preserve and/or disclose member account information and/or content if it is requested to do so by law or in good faith belief that any such action is deemed reasonably necessary for:
+          </p>
+          <p className="post-paragraph">
+            <ol>
+                <li>
+                    compliance with any legal process;
+                </li>
+                <li>
+                    enforcement of the TOS;
+                </li>
+                <li>
+                    responding to any claim that therein contained content is in violation of the rights of any third party;
+                </li>
+                <li>
+                    responding to requests for customer service; or
+                </li>
+                <li>
+                    protecting the rights, property or the personal safety of Yego Coffee, its visitors, users and members, including the general public.
+                </li>
+            </ol>
+          </p>
+          <p className="post-paragraph">
+            Yego Coffee herein reserves the right to include the use of security components that may permit digital information or material to be protected, and that such use of information and/or material is subject to usage guidelines and regulations established by Yego Coffee or any other content providers supplying content services to Yego Coffee. You are hereby prohibited from making any attempt to override or circumvent any of the embedded usage rules in our Services. Furthermore, unauthorized reproduction, publication, distribution, or exhibition of any information or materials supplied by our Services, despite whether done so in whole or in part, is expressly prohibited.
+          </p>
+          
         </div>
       </div>
     </div>
