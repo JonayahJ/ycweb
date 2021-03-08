fc9b4c471e0af700faec7831ce7f402e5903aa6a
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-03-08 16:27:38 -0500

    extra hidden input line for Contact form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "708d7d3714031d81f3bc";
+/******/ 	var hotCurrentHash = "3ba23aeb8d4c5602b360";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Sections/Forms/ContactForm.js
+++ src/components/Sections/Forms/ContactForm.js
@@ -10,40 +10,41 @@ export default function ContactForm(style) {
             <h3 className="contact-title">Get In Touch</h3>
             <div className="contact-form">
                 <form onSubmit={handleSubmit(onSubmit)} name="Contact" method="POST" data-netlify="true" netlify netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="Contact" />
                     <div className="input-validator">
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        ref={register({ required: true })}
-                    />
-                    {errors.name && (
-                        <span className="input-error">Please provide a name</span>
-                    )}
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            ref={register({ required: true })}
+                        />
+                        {errors.name && (
+                            <span className="input-error">Please provide a name</span>
+                        )}
                     </div>
                     <div className="input-validator">
-                    <input
-                        type="text"
-                        name="email"
-                        placeholder="Email"
-                        ref={register({ required: true })}
-                    />
-                    {errors.email && (
-                        <span className="input-error">
-                        Please provide an email
-                        </span>
-                    )}
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Email"
+                            ref={register({ required: true })}
+                        />
+                        {errors.email && (
+                            <span className="input-error">
+                            Please provide an email
+                            </span>
+                        )}
                     </div>
                     <div className="input-validator">
-                    <textarea
-                        name="message"
-                        id=""
-                        cols="30"
-                        rows="3"
-                        placeholder="Message"
-                    />
+                        <textarea
+                            name="message"
+                            id=""
+                            cols="30"
+                            rows="3"
+                            placeholder="Message"
+                        />
                     </div>
-                    <button className="btn -dark" type="submit">SEND MESSAGE</button>
+                    <button className="btn" type="submit">SEND MESSAGE</button>
                 </form>
             </div>
         </div>
