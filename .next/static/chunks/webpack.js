60f66cbc43f0bf20fb23ad2f40e2917824a9ad96
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-03-09 15:08:16 -0500

    bottom buttons on WS pages take 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b9cb5c79fa111cd4a385";
+/******/ 	var hotCurrentHash = "7538c59d7b2c64b65954";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
