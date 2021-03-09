b0216be1ecef1a6ffc9c2068f3b1decd0426e7c2
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-03-09 12:23:48 -0500

    removed other file duplicates, added export script to build command

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ee73ded681dde5a5f456";
+/******/ 	var hotCurrentHash = "0a83ad4912b8f7d009ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
